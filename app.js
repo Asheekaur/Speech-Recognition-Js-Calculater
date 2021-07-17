cc1aecf41cac64e6e3ba01919912610dfcb44963
Asheekaur <asheedhiman55@gmail.com>
2021-07-17 06:45:12 +0530

    keyboard event added

--- app.js
+++ app.js
@@ -95,3 +95,59 @@ clearLast.addEventListener("click", e => {
     display2Element.innerText = "";
     dis2Num = "";
 })
+
+
+// Through keyboard
+window.addEventListener("keydown", e => {
+    if (
+        e.key === "0" ||
+        e.key === "1" ||
+        e.key === "2" ||
+        e.key === "3" ||
+        e.key === "4" ||
+        e.key === "5" ||
+        e.key === "6" ||
+        e.key === "7" ||
+        e.key === "8" ||
+        e.key === "9" ||
+        e.key === "."
+    ) {
+        clickButton(e.key);
+    }
+    else if (
+        e.key === "+" ||
+        e.key === "-" ||
+        e.key === "/" ||
+        e.key === "%"
+    ) {
+        clickOperation(e.key);
+    }
+    else if (e.key === "*") {
+        clickOperation("x");
+    }
+    else if (e.key == "Enter" || e.key === "=") {
+        clickEqual();
+    }
+})
+
+function clickButton(key) {
+    numbers.forEach(button => {
+        if (button.innerText === key) {
+            button.click();
+        }
+    })
+}
+
+
+function clickOperation(key) {
+    operations.forEach(button => {
+        if (button.innerText === key) {
+            button.click();
+        }
+    })
+}
+
+
+function clickEqual() {
+    equal.click();
+}
