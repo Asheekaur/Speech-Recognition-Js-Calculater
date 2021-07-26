ad3d3d88ea4ee47470aae201064235e8fdcceff3
Asheekaur <asheedhiman55@gmail.com>
2021-07-26 23:56:15 +0530

    speech recognition completed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const display1Element = document.querySelector(".display-1");
-const display2Element = document.querySelector(".display-2");
+let display1Element = document.querySelector(".display-1");
+let display2Element = document.querySelector(".display-2");
 const tempResult = document.querySelector(".temp-result");
 const numbers = document.querySelectorAll(".number");
 const operations = document.querySelectorAll(".operation");
@@ -13,7 +13,6 @@ let result = null;
 let lastOperation = "";
 let haveDot = false;
 
-
 numbers.forEach(number => {
     number.addEventListener("click", e => {
         if (e.target.innerText === "." && !haveDot) {
@@ -26,8 +25,6 @@ numbers.forEach(number => {
         console.log(dis2Num);
     })
 })
-
-
 operations.forEach(operation => {
     operation.addEventListener("click", e => {
         if (!dis2Num) return;
@@ -43,7 +40,6 @@ operations.forEach(operation => {
         console.log(dis2Num);
     })
 });
-
 function clearVar(name = "") {
     dis1Num += dis2Num + " " + name + " ";
     display1Element.innerText = dis1Num;
@@ -51,7 +47,6 @@ function clearVar(name = "") {
     dis2Num = " ";
     tempResult.innerText = result;
 }
-
 function mathOperation() {
     if (lastOperation === "x") {
         result = parseFloat(result) * parseFloat(dis2Num);
@@ -68,9 +63,7 @@ function mathOperation() {
     else if (lastOperation === "%") {
         result = parseFloat(result) % parseFloat(dis2Num);
     }
-
 }
-
 equal.addEventListener("click", e => {
     if (!dis1Num || !dis2Num) return;
     haveDot = false;
@@ -81,7 +74,6 @@ equal.addEventListener("click", e => {
     dis2Num = result;
     dis1Num = "";
 })
-
 clear.addEventListener("click", e => {
     display1Element.innerText = "0";
     display2Element.innerText = "0";
@@ -90,13 +82,10 @@ clear.addEventListener("click", e => {
     result = "";
     tempResult.innerText = "0";
 })
-
 clearLast.addEventListener("click", e => {
     display2Element.innerText = "";
     dis2Num = "";
 })
-
-
 // Through keyboard
 window.addEventListener("keydown", e => {
     if (
@@ -129,7 +118,6 @@ window.addEventListener("keydown", e => {
         clickEqual();
     }
 })
-
 function clickButton(key) {
     numbers.forEach(button => {
         if (button.innerText === key) {
@@ -137,8 +125,6 @@ function clickButton(key) {
         }
     })
 }
-
-
 function clickOperation(key) {
     operations.forEach(button => {
         if (button.innerText === key) {
@@ -146,8 +132,49 @@ function clickOperation(key) {
         }
     })
 }
-
-
 function clickEqual() {
     equal.click();
+}
+// Through microphone
+let microphone = document.querySelector(".icon");
+let results = document.getElementById("result");
+microphone.onclick = function () {
+    display1Element.style.display = "none";
+    tempResult.style.display = "none";
+    microphone.classList.add("record");
+    let recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+    recognition.lang = "en-US";
+    recognition.start();
+    operation = {
+        "plus": "+",
+        "minus": "-",
+        "multiply": "*",
+        "multiplied": "*",
+        "divide": "/",
+        "divided": "/",
+        "reminder": "%"
+    }
+    recognition.onresult = function (event) {
+        let input = event.results[0][0].transcript;
+        console.log(input);
+        for (property in operation) {
+            input = input.replace(property, operation[property]);
+        }
+        display2Element.innerText = input;
+        setTimeout(function () {
+            evaluate(input);
+        }, 2000);
+        microphone.classList.remove("record");
+    }
+}
+function evaluate(input) {
+    try {
+        let result = eval(input);
+        display2Element.innerText = result;
+    }
+    catch (e) {
+        alert("It's not a mathematical");
+    }
+    display1Element.style.display = "block";
+    tempResult.style.display = "block";
 }
